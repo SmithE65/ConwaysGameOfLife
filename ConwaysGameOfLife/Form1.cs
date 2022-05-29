9df6fa064713f391668a85bdc129cd3ce3c8c53c
Eric Smith <ydrive65@yahoo.com>
2022-05-28 21:50:37 -0400

    implementation vomit

--- ConwaysGameOfLife/Form1.cs
+++ ConwaysGameOfLife/Form1.cs
@@ -1,52 +1,69 @@
+namespace ConwaysGameOfLife;
+
 using System.Drawing.Drawing2D;
 using Timer = System.Windows.Forms.Timer;
 
-namespace ConwaysGameOfLife
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    private readonly Timer _drawTimer = new();
+    private readonly Matrix _matrix = new();
+    private readonly bool[] _bools = new bool[400*400];
+    private const int width = 400;
+
+    public Form1()
     {
-        private readonly Timer _drawTimer = new();
-        private readonly Matrix _matrix = new();
+        InitializeComponent();
+        DoubleBuffered = true;
+        Paint += Form1_Paint;
+        _drawTimer.Interval = 16;
+        _drawTimer.Enabled = true;
+        _drawTimer.Tick += DrawTimer_Tick;
+        SizeChanged += Form1_SizeChanged;
 
-        public Form1()
+        var rnd = new Random();
+        for (int i = 0; i < _bools.Length; i++)
         {
-            InitializeComponent();
-            DoubleBuffered = true;
-            Paint += Form1_Paint;
-            _drawTimer.Interval = 16;
-            _drawTimer.Enabled = true;
-            _drawTimer.Tick += DrawTimer_Tick;
-            SizeChanged += Form1_SizeChanged;
+            _bools[i] = rnd.Next(2) == 1;
         }
 
-        private void Form1_SizeChanged(object? sender, EventArgs e)
+        UpdateMatronx();
+    }
+
+    private void Form1_SizeChanged(object? sender, EventArgs e)
+    {
+        UpdateMatronx();
+    }
+
+    private void UpdateMatronx()
+    {
+        var playingArea = new Rectangle(0, 0, 400, 400);
+        int clientWidth = ClientRectangle.Width;
+        int clientHeight = ClientRectangle.Height;
+        float ratioX = clientWidth / (float)playingArea.Width;
+        float ratioY = clientHeight / (float)playingArea.Height;
+        float ratio = Math.Min(ratioX, ratioY);
+        _matrix.Reset();
+        _matrix.Translate((ClientRectangle.Width - playingArea.Width * ratio) / 2f,
+            (ClientRectangle.Height - playingArea.Height * ratio) / 2f);
+        _matrix.Scale(ratio, ratio);
+    }
+
+    private void Form1_Paint(object? sender, PaintEventArgs e)
+    {
+        e.Graphics.Transform = _matrix;
+        for (int i = 0; i < _bools.Length; i++)
         {
-            Rectangle playingArea = new Rectangle(0, 0, 400, 400);
-            int width = this.ClientRectangle.Width;
-            int height = this.ClientRectangle.Height;
-            float ratioX = width / playingArea.Width;
-            float ratioY = width /playingArea.Height;
-            float ratio = Math.Min(ratioX, ratioY);
-            _matrix.Reset();
-            _matrix.Translate((ClientRectangle.Width - playingArea.Width * ratio) / 2f,
-                ((ClientRectangle.Height - playingArea.Height * ratio) / 2f));
-            _matrix.Scale(ratio, ratio);
+            e.Graphics.FillRectangle(_bools[i] ? Brushes.CornflowerBlue : Brushes.OldLace, i % width, i / width, 1, 1);
         }
+    }
 
-        private void Form1_Paint(object? sender, PaintEventArgs e)
-        {
-            e.Graphics.DrawString(DateTime.Now.ToString(), this.Font, Brushes.Black, PointF.Empty);
+    private void DrawTimer_Tick(object? sender, EventArgs e)
+    {
+        Invalidate();
+    }
 
-        }
+    private void Form1_Load(object sender, EventArgs e)
+    {
 
-        private void DrawTimer_Tick(object? sender, EventArgs e)
-        {
-            Invalidate();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
