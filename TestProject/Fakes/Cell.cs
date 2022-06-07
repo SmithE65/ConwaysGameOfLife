8ca19a74ef97cf9ca17884641a1ed753c3f50526
Eric Smith <ydrive65@yahoo.com>
2022-06-06 20:53:59 -0400

    updated tests to new ICell interface

---
+++ TestProject/Fakes/Cell.cs
@@ -0,0 +1,17 @@
+﻿using GameOfLife;
+
+namespace TestProject.Fakes;
+
+public class Cell : ICell
+{
+    private readonly bool _isAlive;
+
+    public Cell(bool alive, int liveNeighbors)
+    {
+        _isAlive = alive;
+        AliveNeighbors = liveNeighbors;
+    }
+
+    public int AliveNeighbors { get; init; }
+    public bool IsAlive => _isAlive;
+}
--- TestProject/RulesTests.cs
+++ TestProject/RulesTests.cs
@@ -1,20 +1,7 @@
-using GameOfLife;
 using GameOfLife.Rules;
+using TestProject.Fakes;
 
 namespace TestProject;
-
-public class Cell : ICell
-{
-    private readonly bool _isAlive;
-
-    public Cell(bool alive, List<bool> neighbors)
-    {
-        _isAlive = alive;
-    }
-
-    public int AliveNeighbors { get; init; }
-    public bool IsAlive => _isAlive;
-}
 
 public class RulesTests
 {
@@ -22,7 +9,7 @@ public class RulesTests
     public void SurviveRule_TwoNeighbors_ReturnsTrue()
     {
         // Arrange
-        var cell = new Cell(true, new List<bool> { false, false, true, true });
+        var cell = new Cell(true, 2);
         var sut = new SurvivesRule();
 
         // Act
@@ -36,7 +23,7 @@ public class RulesTests
     public void SurviveRule_OneNeighbor_ReturnsFalse()
     {
         // Arrange
-        var cell = new Cell(true, new List<bool> { true, false, false, false });
+        var cell = new Cell(true, 1);
         var sut = new SurvivesRule();
 
         // Act
